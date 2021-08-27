94bde4adefacf8bd2ffa756d426ea964b91fe369
Glemakk <kovgar.brand@gmail.com>
2021-08-27 17:36:51 +0200

    added background image on Home page

--- src/redux/views/ContactsView/ContactsView.styled.js
+++ src/redux/views/ContactsView/ContactsView.styled.js
@@ -16,3 +16,17 @@ export const ListContainer = styled.div`
   padding-top: 7px;
   padding-left: 45px;
 `
+export const Title = styled.h1`
+  margin-top: 60px;
+  position: absolute;
+  color: #dbdbdb;
+  width: 100%;
+  text-align: center;
+  /* background-image: url('../../../images/jonah-pettrich-V-dfcNnb9OU-unsplash.jpg'); */
+`
+
+export const Img = styled.img`
+  /* opacity: 0.1; */
+  width: 100%;
+  max-width: 100%;
+`
--- src/redux/views/HomeView.js
+++ src/redux/views/HomeView.js
@@ -1,7 +1,22 @@
-const HomeView = () => (
-  <h1>
-    This is your private "Phonebook" where you can safely store, add and safe
-    you contacts
-  </h1>
-)
+// import s from '../../index.css'
+// console.log('s >>', s)
+
+import defaultBackground from '../../images/laptop1.jpg'
+
+import { Title, Img } from './ContactsView/ContactsView.styled'
+
+const HomeView = () => {
+  const background = defaultBackground
+  return (
+    <>
+      <Title>
+        This is your private "Phonebook" where you can safely store, add and
+        safe you contacts
+      </Title>
+      <div>
+        <Img src={background} alt="" width="1200" />
+      </div>
+    </>
+  )
+}
 export default HomeView
